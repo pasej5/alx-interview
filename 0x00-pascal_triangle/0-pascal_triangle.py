4839e34f92f45200c03aa2408984b1127e8aef11
Jealous Matsikachando <pasej5@gmail.com>
2024-01-04 23:11:17 +0200

    rows of triagle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,14 +12,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     else:
-        res = []
+        rows_of_triangle = []
         for i in range(n):
-            if len(res) == 0:
-                res.append([1])
+            if len(rows_of_triangle) == 0:
+                rows_of_triangle.append([1])
             else:
                 row = [1]
-                for j in range(1, len(res[-1])):
-                    row.append(res[-1][j] + res[-1][j - 1])
+                for j in range(1, len(rows_of_triangle[-1])):
+                    row.append(rows_of_triangle[-1][j] + rows_of_triangle[-1][j - 1])
                 row.append(1)
-                res.append(row)
-        return res
+                rows_of_triangle.append(row)
+        return rows_of_triangle
