5a0542fb8de4887611028a2075380d8b2adc3adf
Jealous Matsikachando <pasej5@gmail.com>
2024-01-18 22:32:35 +0200

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def minOperations(n):
+    '''
+    Minimum Operations
+    '''
+    if n <= 1:
+        return 0
+    operations = 0
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            operations += i
+            n //= i
+        i += 1
+    if n > 1:
+        operations += n
+    return operations
