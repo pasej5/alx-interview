a8f49033900aa2b2087cce641ce3efad7991a981
Jealous Matsikachando <pasej5@gmail.com>
2024-05-19 19:56:08 +0200

    final code

--- leetcode_practice/Contains Duplicate/contains_duplicate.py
+++ leetcode_practice/Contains Duplicate/contains_duplicate.py
@@ -3,4 +3,8 @@
 def containsDuplicate(self, nums: List[int]) -> bool:
     """Funstion that checks if the list contains duplicates"""
     hashset = set()
-    for n in nums(n)
+    for n in nums:
+        if n in hashset:
+            return True
+        hash.add(n)
+    return False
