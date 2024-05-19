dc390d8a12249301781e418cb6e2bddad7cc4ac9
Jealous Matsikachando <pasej5@gmail.com>
2024-05-19 19:36:18 +0200

    contains duplicate first commit

---
+++ leetcode_practice/Contains Duplicate/contains_duplicate.py
@@ -0,0 +1,6 @@
+#!/bin/bash/python3
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+    """Funstion that checks if the list contains duplicates"""
+    hashset = set()
+    for n in nums(n)
